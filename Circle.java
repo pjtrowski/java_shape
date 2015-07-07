94cb680af2a35490b4f38866e62a7403f6b0f595
PJ Trowski <ptrowski77@gmail.com>
2015-07-07 14:25:03 -0400

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,18 @@
+public class Circle extends Shape{
+
+	private static final double PI=Math.PI;
+	private double diameter; 
+
+	public Circle(double diameter) {
+		this.diameter=diameter;
+	}
+
+	public double area()
+	{
+		double radius =diameter /2;
+		return PI*radius*radius;
+		
+		
+	}
+
+}
