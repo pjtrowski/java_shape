070e0164f96d2e4c7921eceafe3276de066c64ad
PJ Trowski <ptrowski77@gmail.com>
2015-07-07 14:27:50 -0400

    pjtrowski

---
+++ Rectangle.java
@@ -0,0 +1,20 @@
+public class Rectangle extends Shape{
+	private double a;
+	private double b;
+	
+	public Rectangle(double a,double b)
+	{
+		this.a=a;
+		this.b=b;
+		
+	}
+	
+	public double area()
+	{
+		return a*b;
+		
+	}
+	
+	
+
+}
